795dcdb7340f619fab1c908c5e7f3ba831632fb1
BlackoutForever <denysleybak2453@gmail.com>
2022-10-22 15:24:15 +0300

    v3

--- Blackout.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
+++ Blackout.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -8,7 +8,7 @@ namespace Blackout.TaskPlanner.Domain.Logic
 
         public WorkItem[] CreatePlan(WorkItem[] items)
         {
-            //items = Sort(items);
+            
             List<WorkItem> listItems = items.ToList();
 
             listItems.OrderBy(w => w.Title);
@@ -16,17 +16,6 @@ namespace Blackout.TaskPlanner.Domain.Logic
             listItems.Sort(new TaskComparer());
                     
             return listItems.ToArray();
-        }
-
-        private static WorkItem[] Sort(WorkItem[] workItems)
-        {
-            List<WorkItem> _workItems = workItems.ToList();
-
-            var sortedWorkItems = from w in _workItems
-                                  orderby w.Title
-                                  select w;
-
-            return sortedWorkItems.ToArray();
         }
     }
 }
